623d73e538dd13c685df2f84b87e56347c0aa31f
0xcacti <0xcacti@gmail.com>
2025-07-28 02:14:18 -0400

    add lisp data types

--- include/parser.h
+++ include/parser.h
@@ -4,6 +4,22 @@
 #include "token.h"
 #include "lexer.h"
 
+typedef enum {
+  ATOM,
+  LIST,
+} node_type_t;
+
+typedef struct s_expression {
+  node_type_t type;
+  union {
+    char *atom;
+    struct {
+      struct s_expression **elements;
+      size_t count;
+    } list;
+  } data;
+} s_expression_t; 
+
 typedef struct parser {
   lexer_t *lexer;
   token_t current_token;
@@ -15,5 +31,7 @@ typedef struct parser {
 
 parser_t parser_new(lexer_t *lexer);
 void parser_free(parser_t *parser);
+
+s_expression_t *parse_program(
 
 #endif
