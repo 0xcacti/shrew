269be8c88a1863dacf5964b07d438cd777120bc4
0xcacti <0xcacti@gmail.com>
2025-08-07 01:23:47 -0400

    checkin

--- include/evaluator.h
+++ include/evaluator.h
@@ -1,6 +1,27 @@
 #ifndef EVALUATOR_H
 #define EVALUATOR_H
-#include "parser.h"
 
+#include "parser.h"
+#include "utils.h"
+
+typedef enum {
+  EVAL_OK = 0, 
+  EVAL_ERR = 1,
+} eval_status_t;
+
+typedef struct {
+  eval_status_t status; 
+  s_expression_t *result; 
+  char *error_message; 
+} eval_result_t;
+
+typedef struct env {
+  struct env *parent; 
+  hashtable *store;   
+} env_t;
+
+eval_result_t evaluate_single(s_expression_t *expr, env_t *env);
+eval_result_t evaluatate_many(s_expression_t **exprs, size_t count, env_t *env);
+void evaluator_result_free(eval_result_t *r);
 
 #endif
