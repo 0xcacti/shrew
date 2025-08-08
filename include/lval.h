a590d358f693c293a2eece280edfeda54373b06a
0xcacti <0xcacti@gmail.com>
2025-08-08 16:26:19 -0400

    get it going with lval baby

---
+++ include/lval.h
@@ -0,0 +1,33 @@
+#ifndef LVAL_H
+#define LVAL_H
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef enum {
+  L_NIL,
+  L_BOOL,
+  L_NUM,
+  L_STRING,
+  L_SYMBOL,
+  // add more here later
+} ltype_t;
+
+typedef struct lval {
+  unsigned char mark;
+  ltype_t type;
+  union {
+    double number;
+    bool boolean;
+    struct { char *ptr; size_t len; } string;
+    struct { const char *name; } symbol;
+  } as;
+} lval_t;
+
+lval_t *lval_num(double x);
+lval_t *lval_bool(bool b);
+lval_t *lval_string_copy(const char *s, size_t len);
+lval_t *lval_intern(const char *name);
+const char *lval_type_name(const lval_t *v);
+void lval_print(const lval_t *v);
+
+#endif 
--- src/env.c
+++ src/env.c
@@ -1,3 +1,28 @@
 #include "env.h"
 #include "hashtable.h"
 #include "parser.h"
+#include <stdlib.h>
+
+bool env_init(env_t *env, env_t *parent) {
+  if (!env)
+    return false;
+  env->parent = parent;
+  env->store = (struct hashtable *)malloc(sizeof *env->store);
+  if (!env->store)
+    return false;
+
+  hashtable ht;
+  ht_error err = {0};
+  ht_init(&ht, 16, &err);
+
+  env->parent = parent;
+  env->store = &ht;
+}
+
+// Test(hashtable, init_and_destroy) {
+//   hashtable t;
+//   ht_error err = {0};
+//   cr_assert(ht_init(&t, 16, &err), "init failed: %s", err.error_message);
+//   cr_assert_eq(ht_count(&t), 0, "new table should be empty");
+//   ht_destroy(&t);
+// }
