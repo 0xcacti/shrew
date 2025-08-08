390981f213c9ecbff982957a2c0295319d044846
0xcacti <0xcacti@gmail.com>
2025-08-08 16:50:33 -0400

    take a lil break I'm frustrated

--- src/lval.c
+++ src/lval.c
@@ -19,7 +19,22 @@ lval_t *lval_bool(bool b) {
   return v;
 }
 
-// lval_t *lval_bool(bool b) {}
+lval_t *lval_string_copy(const char *s, size_t len) {
+  lval_t *v = malloc(sizeof(lval_t));
+  if (!v) return NULL;
+  v->mark = 0; // 0 for now when created
+  v->type = L_STRING;
+  v->as.string.len = len;
+  v->as.string.data = malloc(len + 1);
+  if (!v->as.string.data) {
+    free(v);
+    return NULL;
+  }
+  memcpy(v->as.string.data, s, len);
+  v->as.string.data[len] = '\0'; // null-terminate
+  return v;
+}
+
 // lval_t *lval_string_copy(const char *s, size_t len) {}
 // lval_t *lval_intern(const char *name) {}
 // const char *lval_type_name(const lval_t *v) {}
