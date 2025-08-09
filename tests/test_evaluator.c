4b8c3b3c3c237576a240d17e20690d0b4b25a618
0xcacti <0xcacti@gmail.com>
2025-08-09 03:13:18 -0400

    boom we are evaluating

--- tests/test_evaluator.c
+++ tests/test_evaluator.c
@@ -39,3 +39,28 @@ Test(evaluator_tests, evaluate_number_atom) {
   env_destroy(&env);
   symbol_intern_free_all();
 }
+
+Test(evaluator_tests, evaluate_string_atom) {
+  symbol_intern_init();
+
+  env_t env;
+  cr_assert(env_init(&env, NULL));
+
+  parser_t parser = { 0 };
+  parse_result_t pr = setup_input("\"Hello, World!\"", &parser);
+
+  s_expression_t *expr = pr.expressions[0];
+  eval_result_t res = evaluate_single(expr, &env);
+
+  cr_assert_eq(res.status, EVAL_OK);
+  cr_assert_not_null(res.result);
+  cr_assert_eq(res.result->type, L_STRING);
+  cr_assert_str_eq(res.result->as.string.ptr, "Hello, World!");
+
+  lval_free(res.result);
+  evaluator_result_free(&res);
+  parse_result_free(&pr);
+  parser_free(&parser);
+  env_destroy(&env);
+  symbol_intern_free_all();
+}
