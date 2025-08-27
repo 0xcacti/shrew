bdb4ece4f4c570b189dfa419c4db3a7e0ebf687d
0xcacti <0xcacti@gmail.com>
2025-08-26 22:40:11 -0400

    I have no idea how to do gc

--- include/gc.h
+++ include/gc.h
@@ -11,6 +11,12 @@ void gc_init(struct env *global_env);
 void gc_set_global_env(struct env *global_env);
 struct lval *gc_alloc_lval();
 void gc_collect(struct lval *extra_root);
+
+
+void gc_root(struct lval **slot);
+void gc_unroot(struct lval **slot);
+size_t gc_object_count(void);
+void gc_set_trigger(size_t threshold);
 void env_gc_mark_all(struct env *e, void (*mark)(struct lval *));
 void gc_root_push(struct lval **slot);
 size_t gc_root_pop();
